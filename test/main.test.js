533e1e5daeaa8321e7156d32362da1df26754220
cwxyz <jie844067636@gmail.com>
2017-06-03 10:22:30 +0800

    [wangjie&&liuhui] -- complete second rule

--- test/main.test.js
+++ test/main.test.js
@@ -13,4 +13,10 @@ describe('cellLifeGame', () => {
         expect(cellLifeGame([[0, 1, 0], [0, 1, 0], [1, 0, 0]])).equal(1);
         expect(cellLifeGame([[0, 0, 0], [0, 0, 0], [1, 0, 1]])).equal(0);
     });
+
+    it('should return death(0) if cell around is more than three', () => {
+        expect(cellLifeGame([[1, 1, 1], [0, 1, 0], [0, 0, 0]])).equal(0);
+        expect(cellLifeGame([[0, 1, 0], [0, 1, 0], [1, 1, 0]])).equal(0);
+        expect(cellLifeGame([[1, 1, 1], [0, 1, 0], [1, 1, 1]])).equal(0);
+    });
 });
