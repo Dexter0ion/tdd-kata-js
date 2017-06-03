ea253249f762a8ca1cf9340da25ed5f4e370524b
cwxyz <jie844067636@gmail.com>
2017-06-03 10:21:07 +0800

    [wangjie&&liuhui] -- complete second rule

--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
 import _ from 'lodash'
 
-const cellLifeGame = (cellArr) => {
-  let aliveAmount = 0;
-  if(cellArr[1][1]===1){
-    for(let i=0;i<3;i++){
-        for(let j=0;j<3;j++){
-          if(cellArr[i][j]===1){
-            aliveAmount++;
-          }
+const CELLROW = 3;
+
+/**
+ * @return {number}
+ */
+function GetCellAliveAmount(cellArr) {
+    let aliveAmount = 0;
+    for (let i = 0; i < CELLROW; i++) {
+        for (let j = 0; j < CELLROW; j++) {
+            if (cellArr[i][j] === 1) {
+                aliveAmount++;
+            }
         }
     }
-    if(aliveAmount<=2){
-      cellArr[1][1]=0;
-      return cellArr[1][1];
+    return aliveAmount;
+}
+
+const cellLifeGame = (cellArr) => {
+    let aliveAmount = GetCellAliveAmount(cellArr);
+
+    if (cellArr[1][1] === 1) {
+        if (aliveAmount <= 2) {
+            return 0;
+        } else if (aliveAmount <= 4) {
+            return cellArr[1][1];
+        }
+    } else {
+        if (aliveAmount >= 2 && aliveAmount <= 3) {
+            return cellArr[1][1];
+        }
     }
-  }
 };
 
 module.exports = {
-  cellLifeGame
+    cellLifeGame
 };
--- test/main.test.js
+++ test/main.test.js
@@ -5,6 +5,12 @@ describe('cellLifeGame', () => {
     it('should return death (0) if less than two cell alive around this cell', () => {
         expect(cellLifeGame([[1, 0, 0], [0, 1, 0], [0, 0, 0]])).equal(0);
         expect(cellLifeGame([[0, 1, 0], [0, 1, 0], [0, 0, 0]])).equal(0);
-        expect(cellLifeGame([[0, 0, 1], [0, 1, 0], [0, 0, 0]])).equal(0);
+        expect(cellLifeGame([[0, 0, 0], [0, 1, 0], [0, 0, 0]])).equal(0);
+    });
+
+    it('should return the same state if cell around is between two and three', () => {
+        expect(cellLifeGame([[1, 0, 1], [0, 1, 0], [0, 0, 0]])).equal(1);
+        expect(cellLifeGame([[0, 1, 0], [0, 1, 0], [1, 0, 0]])).equal(1);
+        expect(cellLifeGame([[0, 0, 0], [0, 0, 0], [1, 0, 1]])).equal(0);
     });
 });
